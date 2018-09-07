3ade01b75f105af419e868848c8df847bced03d3
Fede Vargas <fedevargas110@gmail.com>
2018-09-07 13:46:29 -0300

    se anaden los post

--- ToDoList/urls.py
+++ ToDoList/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.mostrar, name="home"),
+    path('post', views.post, name="post"),
 ]
--- ToDoList/views.py
+++ ToDoList/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, redirect
 from ToDoList.models import *
+from .forms import *
 
 def mostrar(request):
-    list = Lista.objects.all()
-    ctx = {'list' : list}
+    list = Lista.objects.order_by('-fecha')
+    form = ListaForm()
+    ctx = {'list' : list, 'form' : form}
     return render(request, "base.html", ctx)
 
+def post(request):
+    texto = request.POST.get("texto")
+    post = Lista(texto=texto)
+    post.save()
+    return redirect("home")
 # Create your views here.
